16e3883e591d39515153034375f5bba121e4d2fb
Arash Saheli <arash.saheli@article.com>
2024-06-03 20:12:49 -0700

    init

---
+++ app/components/BackgroundImage.tsx
@@ -0,0 +1,82 @@
+import { Image } from "@shopify/hydrogen";
+import type {
+  InspectorGroup,
+  PositionInputValue,
+  WeaverseImage,
+} from "@weaverse/hydrogen";
+import type { CSSProperties } from "react";
+
+export type BackgroundImageProps = {
+  backgroundImage: WeaverseImage | string;
+  backgroundFit: CSSProperties["objectFit"];
+  backgroundPosition: PositionInputValue;
+};
+
+export function BackgroundImage(props: BackgroundImageProps) {
+  let { backgroundImage, backgroundFit, backgroundPosition } = props;
+  if (backgroundImage) {
+    let data =
+      typeof backgroundImage === "string"
+        ? { url: backgroundImage, altText: "Section background" }
+        : backgroundImage;
+    return (
+      <Image
+        className="absolute inset-0 w-full h-full z-[-1]"
+        data={data}
+        sizes="auto"
+        style={{
+          objectFit: backgroundFit,
+          objectPosition: backgroundPosition,
+        }}
+      />
+    );
+  }
+  return null;
+}
+
+export let backgroundInputs: InspectorGroup["inputs"] = [
+  {
+    type: "select",
+    name: "backgroundFor",
+    label: "Background for",
+    configs: {
+      options: [
+        { value: "section", label: "Section" },
+        { value: "content", label: "Content" },
+      ],
+    },
+    defaultValue: "section",
+  },
+  {
+    type: "color",
+    name: "backgroundColor",
+    label: "Background color",
+    defaultValue: "",
+  },
+  {
+    type: "image",
+    name: "backgroundImage",
+    label: "Background image",
+  },
+  {
+    type: "select",
+    name: "backgroundFit",
+    label: "Background fit",
+    configs: {
+      options: [
+        { value: "fill", label: "Fill" },
+        { value: "cover", label: "Cover" },
+        { value: "contain", label: "Contain" },
+      ],
+    },
+    defaultValue: "cover",
+    condition: "backgroundImage.ne.nil",
+  },
+  {
+    type: "position",
+    name: "backgroundPosition",
+    label: "Background position",
+    defaultValue: "center",
+    condition: "backgroundImage.ne.nil",
+  },
+];
---
+++ app/sections/countdown/index.tsx
@@ -0,0 +1,44 @@
+import type { HydrogenComponentSchema } from "@weaverse/hydrogen";
+import { forwardRef } from "react";
+import type { SectionProps } from "~/components/Section";
+import { Section, sectionInspector } from "~/components/Section";
+
+type CountdownProps = SectionProps;
+
+let Countdown = forwardRef<HTMLElement, CountdownProps>((props, ref) => {
+  let { children, ...rest } = props;
+  return (
+    <Section ref={ref} {...rest} containerClassName="text-center">
+      {children}
+    </Section>
+  );
+});
+
+export default Countdown;
+
+export let schema: HydrogenComponentSchema = {
+  type: "countdown",
+  title: "Countdown",
+  toolbar: ["general-settings", ["duplicate", "delete"]],
+  inspector: sectionInspector,
+  childTypes: ["heading", "subheading", "countdown--timer", "button"],
+  presets: {
+    children: [
+      {
+        type: "heading",
+        content: "Sale ends in",
+      },
+      {
+        type: "subheading",
+        content:
+          "Insert the time limit or an encouraging message of your marketing campaign to create a sense of urgency for your customers.",
+      },
+      {
+        type: "countdown--timer",
+      },
+      {
+        type: "button",
+      },
+    ],
+  },
+};
